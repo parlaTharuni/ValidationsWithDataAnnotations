8ed19c6972e994219964f9a241b1cace36d2240f
parlaTharuni <tharuniparla@gmail.com>
2023-11-24 18:16:50 +0530

    Add project files.

---
+++ ValidationsWithDataAnnotations/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationsWithDataAnnotations.Models
+{
+    public class Student
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Class { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+
+        [Required]
+        [Range(5, 14)]
+        public int Age { get; set; }
+
+
+    }
+}
